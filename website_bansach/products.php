fb4ba7db096bacb0028f3c5d7b09c7f77d32d8dd
minhanh19it5 <minhanh19it5@gmail.com>
2023-03-24 22:01:37 +0700

    first commit

---
+++ website_bansach/products.php
@@ -0,0 +1,85 @@
+<?php
+include 'inc/header.php';
+include 'inc/slider.php';
+ ?>	
+
+ <div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="heading">
+    		<h3>Sách triết học</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+	      <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="preview-3.php"><img src="images/b18.jpg" alt="" /></a>
+					 <h2>Đất Trời Vần Vũ </h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">145.000đ</span></p>
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview-2.php"><img src="images/b19.jpg" alt="" /></a>
+					 <h2>Sát Nhân Mạng</h2>
+					<p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">245.000đ</span></p> 
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview-4.php"><img src="images/b2.jpg" alt="" /></a>
+					 <h2>Sống Sót Ở Berlin </h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">125.000đ</span></p>
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<img src="images/b21.jpg" alt="" />
+					 <h2>Lược Sử Loài Người</h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">145.000đ</span></p> 
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Kỹ năng sống</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="preview-3.php"><img src="images/b3.jpg" alt="" /></a>
+					 <h2>Trên Đường Băng </h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">145.000đ</span></p> 
+				    
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview-4.php"><img src="images/b23.jpg" alt="" /></a>
+					 <h2>Tư Duy Nhanh Và Chậm </h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">145.000đ</span></p> 
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview-2.php"><img src="images/b24.jpg" alt="" /></a>
+					 <h2>Tuổi Trẻ Đáng Giá Bao Nhiêu?  </h2>
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>
+					 <p><span class="price">145.000đ</span></p> 
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/b25.jpg" alt="" />
+					 <h2>Chúng Ta Sống Có Vui Không?</h2>	
+					 <p>Tôi viêt cuốn sách vì thấy người ta không biết trân trọng cảm xúc...</p>				 
+					 <p><span class="price">145.000đ</span></p>    
+				     <div class="button"><span><a href="preview.php" class="details">Chi tiết</a></span></div>
+				</div>
+			</div>
+    </div>
+ </div>
+<?php
+include 'inc/footer.php';
+ ?>
